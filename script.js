8d0d4e1c1fab7e751e050e5784792f03e6420384
IvanBitkin <hanslionx@gmail.com>
2017-09-02 17:43:02 +0300

    fixed plus in index.html ,added function plus

---
+++ script.js
@@ -0,0 +1,26 @@
+function plus(){
+  var number1, number2, result;
+
+  number1 = document.getElementById('num1').value;
+  number1 = parseInt(number1);
+
+  number2 = document.getElementById('num2').value;
+  number2 = parseInt(number2);
+
+  result = number1 + number2;
+
+  document.getElementById('answer').innerHTML = result;
+
+}
+
+function minus(){
+
+}
+
+function multiply(){
+
+}
+
+function division(){
+
+}
