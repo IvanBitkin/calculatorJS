337653f0b78a94b2c4b2fc1f83636f0641fb0c88
IvanBitkin <hanslionx@gmail.com>
2017-09-02 17:53:32 +0300

    added function division

--- script.js
+++ script.js
@@ -43,5 +43,15 @@ function multiply(){
 }
 
 function division(){
+  var number1, number2, result;
 
+  number1 = document.getElementById('num1').value;
+  number1 = parseInt(number1);
+
+  number2 = document.getElementById('num2').value;
+  number2 = parseInt(number2);
+
+  result = number1 / number2;
+
+  document.getElementById('answer').innerHTML = result;
 }
